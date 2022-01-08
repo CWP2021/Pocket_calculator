ce9d4c6757958994f0a1682b1a7a7ff3824c9336
CWP2021 <96430439+CWP2021@users.noreply.github.com>
2022-01-08 15:28:02 +0100

    Update Simple_pocket_calculator.py

--- Simple_pocket_calculator.py
+++ Simple_pocket_calculator.py
@@ -1,66 +1,39 @@
+#Description: A simple pocket calculator that can perform basic arithmetical operations. It will warn the user if they want to divide by zero or if they type in invalid values.
+
 while True:
 
     try:
-
         Continue = input("Continue? y(es); n(o)")
-
         if Continue == "y":
-
             number1 = input("number1:")
-
             operation = input("operation:")
-
             number2 = input("number2:")
-
             number1 = float(number1)
-
             number2 = float(number2)
-
             def add(opt1=number1, opt2=number2):
-
                 return (opt1 + opt2)
-
             def sub(opt1=number1, opt2=number2):
-
                 return (opt1 - opt2)
-
             def mul(opt1=number1, opt2=number2):
-
                 return (opt1 * opt2)
-
             def div(opt1=number1, opt2=number2):
-
                 return (opt1 / opt2)
-
             if operation == "+":
-
                 print(add(opt1=number1, opt2=number2))
-
             elif operation == "-":
-
                 print(sub(opt1=number1, opt2=number2))
-
             elif operation == "*":
-
                 print(mul(opt1=number1, opt2=number2))
-
             elif operation == "/":
-
                 print(div(opt1=number1, opt2=number2))
-
             else:
-
                 print('Only (+), (-), (*), (/)')
-
         else:
-
             print('Program closed')
-
             break
 
     except ValueError:
-
         print("Only numeric values!")
+        
     except ZeroDivisionError:
-
         print('Cannot divide by 0!')
